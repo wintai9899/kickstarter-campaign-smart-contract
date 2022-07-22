7f45cfb46f393973c4a16540bc2d0c51fa279380
wintai9899 <62014141+wintai9899@users.noreply.github.com>
2022-07-22 11:56:46 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,16 +1,13 @@
-//https://github.com/fridays/next-routes/blob/master/README.md
+// returns a function so ()() will immediately call the function
+const routes  = require('next-routes')();
 
-// server.js
-const next = require('next')
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const handler = routes.getRequestHandler(app)
+//route mapping
+//route.add('/directory/:wildcard')
+//redirects to /campaigns/show when address is clicked
 
-// Without express
-const {createServer} = require('http')
-app.prepare().then(() => {
-  createServer(handler).listen(3000, (err) => {
-    if(err) throw err;
-    console.log('Ready on localhost:3000');
-  })
-})
+
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    .add('/campaigns/:address', '/campaigns/show');
+
+module.exports = routes;
